3fa62588394c142f1395eee116db2a39c73d0016
Joshua Bangle <joshua.bangle@gmail.com>
2019-11-02 22:54:40 +0000

    added rubocop, cleaned up code with rubocop automated -a and -x commands

--- spec/controllers/static_page_controller_spec.rb
+++ spec/controllers/static_page_controller_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StaticPageController, type: :controller do
   describe 'static_page#index action' do
-    it "should successfully show the page" do
+    it 'should successfully show the page' do
       get :index
       expect(response).to have_http_status(:success)
-    end  
+    end
   end
 end
