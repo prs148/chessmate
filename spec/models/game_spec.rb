e6815c1a919c87fe5824238517b5048d3649def0
Nickolas Vu <nickvu71@gmail.com>
2019-11-09 22:19:51 +0000

    unfinalized obsructed

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "should detect obstructions" do
+    game = Game.create
+    piece = Piece.new
+    piece.x_position = 1
+    piece.y_position = 1
+    piece.game_id = game.id
+    piece.save
+
+    expect(game.is_obstructed?(0, 0, 2, 2)).to eq true
+    expect(game.is_obstructed?(0, 0, 0, 4)).to eq false
+  end  
 end
