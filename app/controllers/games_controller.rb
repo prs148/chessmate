fb1fa7b12ef931a90435c1556f1e45aebc50b8cd
Pete <prs148@hotmail.com>
2019-11-21 00:09:37 +0000

    added new game on controller and link to start new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,24 @@ class GamesController < ApplicationController
 
   
   def index
-
+  @games = Game.all 
   end
 
+  def new 
+  @game = Game.new 
+  end   
+
+  def create 
+  @game = current_user.game.create(game_params)
+end
+
+  private
+
+  def game_params 
+    params.require(:game).permit(:name, :white_player_id, :black_player_id)
+  end
 
 end
+
+
+  
