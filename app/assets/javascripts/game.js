e4284524a13895b8a6ccfe1acdf2e788d0fe24c7
Joshua Bangle <joshua.bangle@gmail.com>
2019-11-21 02:40:43 +0000

    adjusted table to have split up coords

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -4,15 +4,16 @@ var $table = document.createElement("table");
 
 function buildBoard() {
 
-  for (var i = 0; i < 8; i++) {   
+  for (var x = 0; x < 8; x++) {   
       var $tr = document.createElement('tr');
-      for (var n = 0; n < 8; n++) {
+      for (var y = 0; y < 8; y++) {
           var $td = document.createElement('td');
-          $td.setAttribute('data-coord', i+','+n) //grabs the row iterator (i) and the cell iterator (n) and uses them for the coords! -JB
-          if (i%2 == n%2) {           //every even square is white, odd is black -JB
-              $td.className = "white";
-          } else {
+          $td.setAttribute('data-x', x)
+          $td.setAttribute('data-y', y) //grabs the row iterator (i) and the cell iterator (n) and uses them for the coords! -JB
+          if (x%2 == y%2) {           //every even square is white, odd is black -JB
               $td.className = "black";
+          } else {
+              $td.className = "white";
           }
           $tr.appendChild($td);
       }
