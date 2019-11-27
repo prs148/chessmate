cf33d237985517afb2b1ead3a60f29789064e9d7
Joshua Bangle <joshua.bangle@gmail.com>
2019-11-27 22:11:05 +0000

    adjusted board logic

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,25 +1,68 @@
 $(document).on('turbolinks:load', function() {
-var $boardContainer = document.querySelector('.board-container'); //grabs the div added to the index page -JB
-var $table = document.createElement("table"); 
+  var $boardContainer = document.querySelector('.board-container'); //grabs the div added to the index page -JB
 
-function buildBoard() {
 
-  for (var x = 0; x < 8; x++) {   
-      var $tr = document.createElement('tr');
-      for (var y = 0; y < 8; y++) {
-          var $td = document.createElement('td');
-          $td.setAttribute('data-x', x)
-          $td.setAttribute('data-y', y) //grabs the row iterator (i) and the cell iterator (n) and uses them for the coords! -JB
-          if (x%2 == y%2) {           //every even square is white, odd is black -JB
-              $td.className = "black";
-          } else {
-              $td.className = "white";
+
+
+  function genCoords() {
+      var coords = [];
+      for (var x = 0; x < 8; x++) {    
+          for (var y = 0; y < 8; y++) {
+              coords.push({x, y, className: x%2 == y%2 ? 'black':'white'})
           }
-          $tr.appendChild($td);
       }
-      $table.appendChild($tr);
+      return coords
+    }
+
+
+  function updateCoords() {
+    var coords = genCoords();
+    return coords.map(function(coord){ 
+      if (coord.x === 0 && coord.y === 0 || coord.x === 0 && coord.y === 7) {
+        coord.piece = `rook-${coord.x>2 ? 'black' : 'white'}`;
+      } else if (coord.x === 0 && coord.y === 1 || coord.x === 0 && coord.y === 6) {
+        coord.piece = `knight-${coord.x>2 ? 'black' : 'white'}`;
+      } else if (coord.x === 0 && coord.y === 2 || coord.x === 0 && coord.y === 5) {
+        coord.piece = `bishop-${+ coord.x>2 ? 'black' : 'white'}`;
+      } 
+      return coord;
+    });
   }
-  $boardContainer.appendChild($table);
-}
-buildBoard();
+
+ function buildBoard() {
+    var coords = updateCoords();
+    var $table = document.createElement("table"); 
+    var $tr = document.createElement('tr');
+    var $td = document.createElement('td');
+    $boardContainer.appendChild($table);
+    for (var x = 0; x < 8; x++) { 
+        $table.appendChild($tr);
+      }
+    coords.forEach(function(coord){
+
+    })
+    // for (var y = 0; y < 8; y++) {
+        var $td = document.createElement('td');
+
+        coords.push({x, y}) //adding the coordinates to coords array
+        
+        }
+        
+    }
+    
+  }
+
+
+  
+
+  buildBoard();
+
+  // console.log(buildCoords());
+
 });
+//   function appendPiece(x, y) {
+//     if (x === 1 || x === 6){
+//       pawn = Pawn.new(x, y, white)
+
+//     }
+// }
