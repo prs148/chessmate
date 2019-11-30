613d1a17eeae885c5081c6cde73d4b8bf55aeb6b
Pete <prs148@hotmail.com>
2019-11-30 21:20:05 +0000

    pawn and queen model updated, passing rpsec

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,23 +1,30 @@
 class Pawn < Piece 
-attr_accessor: first_move
-  
-  def initilizer(x, y, color)
-    self.coord_x = x
-    self.coord_y = y
 
-  def is_valid_move?(x, y)
-    valid_direction?(x, y) && !is_obstructed?(x, y) 
-  end
 
-  def valid_direction?(x, y)
-    if !@first_move && ((y == 2 || y == 1) && x = 0)
-      @first_move = true
+def valid_move?(x, y)
+  super(x, y)
+   
+
+    if ((y_position == 1 || y_position == 6) && (y - y_position).abs <= 2 && x == 0)
       return true
-    elsif @first_move && (y == 1 && x == 0)
+    elsif ((y - y_position).abs == 1 && x == 0)
       return true 
-    else 
+    else
       return false
     end
   end
-
 end
+
+
+#   def valid_move?(x, y)
+#     super(x,y)
+#       if @first_move && ((y == 2 || y == 1) && x = 0)
+#         @first_move = true
+#         return true
+#       elsif !@first_move && (y == 1 && x == 0)
+#         return true 
+#       else 
+#         return false
+#       end
+#     end
+# end
