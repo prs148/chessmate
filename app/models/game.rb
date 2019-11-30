e965cb68b072c5c1fe09eca345b9f127e76e91cc
Pete <prs148@hotmail.com>
2019-11-30 23:23:55 +0000

    populate_game method added for pawn creation

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,30 @@ class Game < ApplicationRecord
   scope :available, -> { where(white_player_id: nil).or(where(black_player_id: nil))}
   belongs_to :user
   has_many :pieces
+  after_create :populate_game
+
+
+  def populate_game
+    Pawn.create(game_id: id, x_position: 1, y_position: 1, color: "white", status: true)
+
+  end
+
 
   def piece_at(x, y)
     pieces.where(x_position: x, y_position: y).take
   end
+
+
+  # def color 
+  #   if black_player_id
+  #     return true 
+
+  #   else 
+  #     return false 
+
+
+  # end
+
+
+ 
 end
