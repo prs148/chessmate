d43691fcb3e8b118eef2ddd770117e0ececc03e4
Pete <prs148@hotmail.com>
2019-12-02 04:47:54 +0000

    populate game method added to game.rb also migrations made to include color and status for each piece)

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,23 +1,3 @@
 class Pawn < Piece 
-attr_accessor: first_move
-  
-  def initilizer(x, y, color)
-    self.coord_x = x
-    self.coord_y = y
-
-  def is_valid_move?(x, y)
-    valid_direction?(x, y) && !is_obstructed?(x, y) 
-  end
-
-  def valid_direction?(x, y)
-    if !@first_move && ((y == 2 || y == 1) && x = 0)
-      @first_move = true
-      return true
-    elsif @first_move && (y == 1 && x == 0)
-      return true 
-    else 
-      return false
-    end
-  end
 
 end
