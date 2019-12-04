9c5ffcc6d29eb77cad2e7abdbc28593d94bfd6c8
Nickolas Vu <nickvu71@gmail.com>
2019-12-04 23:19:51 +0000

    add knight valid move

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,4 +1,7 @@
 class Knight < Piece
-
-
+  def valid_move?(x, y)
+    super(x,y) &&
+      [(x - x_position).abs, (y - y_position).abs].sort == [1,2]
+  end
 end
+
---
+++ spec/models/knight_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe Knight, type: :model do
+  before(:all) do
+    @white_player = create(:user)
+    @black_player = create(:user)
+  end
+  before(:each) do
+    @game = create(:game)
+    @game.white_player_id = @white_player.id
+    @game.black_player_id = @black_player.id
+    @game.save
+  end
+
+  context 'movement tests' do
+    it "should move in all valid L positions" do
+      knight = create_piece_for_game(Knight, 4, 4)
+      expect(knight.valid_move?(6, 5)).to eq(true)
+      expect(knight.valid_move?(6, 3)).to eq(true)
+      expect(knight.valid_move?(2, 5)).to eq(true)
+      expect(knight.valid_move?(2, 3)).to eq(true)
+      expect(knight.valid_move?(5, 6)).to eq(true)
+      expect(knight.valid_move?(3, 6)).to eq(true)
+      expect(knight.valid_move?(5, 2)).to eq(true)
+      expect(knight.valid_move?(3, 2)).to eq(true)
+    end
+
+    it "shouldn't be able to be obstructed" do
+      knight = create_piece_for_game(Knight, 4, 4)
+      (0..7).each do |x|
+        create_piece_for_game(Pawn, x, 5)
+      end
+      expect(knight.valid_move?(5, 6)).to eq(true)
+      expect(knight.valid_move?(3, 6)).to eq(true)
+    end
+
+    it "should not move in a straight line" do
+      knight = create_piece_for_game(Knight, 4, 4)
+      expect(knight.valid_move?(4, 7)).to eq(false)
+      expect(knight.valid_move?(7, 4)).to eq(false)
+      expect(knight.valid_move?(1, 4)).to eq(false)
+      expect(knight.valid_move?(4, 0)).to eq(false)
+    end
+  end
+
+  def create_piece_for_game(type, xpos, ypos, player_id = @white_player.id)
+    @game.pieces << type.create(x_position: xpos,
+                                y_position: ypos,
+                                player_id: player_id)
+    @game.pieces.last
+  end
+end
